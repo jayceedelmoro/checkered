437811c97d58dd28c025a6414d83a61c4e856f93
Carlo <jancarlodelmoro@gmail.com>
2024-01-11 20:33:06 +0800

    Feat: Added reusable controller functions

---
+++ backend/services/controllerServices.js
@@ -0,0 +1,74 @@
+const toTitleCase = (str) => {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substring(1, txt.length - 1).toLowerCase();
+    }
+  );
+}
+
+const controllerServices = {
+  getDataById: async (request, response, model) => {
+    try {
+      const { id } = request.params;
+      const modelName = model.collection.collectionName
+      const result = await model.findById(id);
+
+      const status = result ? 200 : 404;
+      const message = result
+        ? result
+        : `${ toTitleCase(modelName) } Does Not Exist`;
+
+      return response.status(status).json({ message: message });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  changeInfo: async (request, response, model) => {
+    try {
+      const { id } = request.params;
+      const modelName = model.collection.collectionName
+
+      const result = await model.findByIdAndUpdate(
+        id,
+        request.body,
+        {
+            new: true
+        }
+      );
+
+      const status = result ? 200 : 404;
+      const message = result
+        ? "Update Success"
+        : `${ toTitleCase(modelName) } Does Not Exist`;
+
+      return response.status(status).json({ message: message });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  deleteData: async (request, response, model) => {
+    try {
+      const { id } = request.params;
+      const modelName = model.collection.collectionName
+
+      const result = await model.findOneAndUpdate({ _id: id }, { isDeleted: true });
+
+      const status = result ? 200 : 404;
+      const message = result
+        ? `${ toTitleCase(modelName) } deleted`
+        : `${ toTitleCase(modelName) } Does Not Exist`;
+
+      return response.status(status).json({ message: message });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ message: "Internal server error" });
+    }
+  },
+};
+
+module.exports = controllerServices;
