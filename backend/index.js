c77a6a576c55718d54ebb1af339ec5b57cf0592f
Carlo <jancarlodelmoro@gmail.com>
2024-01-11 14:24:36 +0800

    Feat: Initial Structure of index.js

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 8000;
+const baseURL = '/api/v1';
+
+//mongoose config
+mongoose.connect(process.env.MONGDB_URI);
+
+const app = express();
+
+app.use(cors({
+    origin: ["http://localhost:3006", "https://checkered.onrender.com"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+
+app.use( helmet() );
+app.use( bodyParser.json() );
+app.use( morgan('dev') );
+
+//Routes
+const UserRoutes = require('./routes/user');
+
+app.use( `${baseURL}/users`, UserRoutes );
+
+app.get('/', ( request, response ) => {
+    response.status(200).send( `` );
+});
+
+app.listen( PORT, () => { console.log( `Server currently running on port ${PORT}` ) });
