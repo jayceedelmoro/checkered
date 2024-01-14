68f10b7531fe73c0901bec34719d603c3d810281
Carlo <jancarlodelmoro@gmail.com>
2024-01-14 13:58:38 +0800

    Feat: Added Modal to Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom';
 
 import TaskComponent from '../components/TaskComponent';
+import ModalComponent from '../components/ModalComponent';
 
 import '../styling/Dashboard.scss'
 
 const Dashboard = () => {
     
 const [ taskData, setTaskData ] = useState( useLoaderData() );
+
+const [ isModalOpen, setIsModelOpen] = useState(false);
+
+const modalToggle = () => {
+    setIsModelOpen(!isModalOpen);
+}
 
   return (
     <>
@@ -17,7 +24,7 @@ const [ taskData, setTaskData ] = useState( useLoaderData() );
                 <div class="add-btn-container">
                     <button
                         class="add-task"
-                        onClick={"modalToggle"}
+                        onClick={ modalToggle }
                     >
                         Add Task
                     </button>
@@ -37,6 +44,12 @@ const [ taskData, setTaskData ] = useState( useLoaderData() );
                 taskData = { taskData }
                 setTaskData = { setTaskData }
             />
+            
+            {
+                isModalOpen
+                ? <ModalComponent modalToggle = { modalToggle }/>
+                : null
+            }
         </div>
     </>
   )
