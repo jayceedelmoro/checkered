768fb8c89de9692df77ecd2205c9ed761d217b63
Carlo <jancarlodelmoro@gmail.com>
2024-01-14 14:46:53 +0800

    Feat: Added buttons on Modal

--- frontend/src/components/ModalComponent.jsx
+++ frontend/src/components/ModalComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 import { CloseCircleOutline } from '@ricons/ionicons5';
@@ -7,6 +7,19 @@ import { Icon } from '@ricons/utils'
 import '../styling/ModalComponent.scss'
 
 const ModalComponent = (props) => {
+
+    const [ state, setState ] = useState({});
+    const { value } = state;
+
+    //general function to change value of inputs
+    const changeText = (e) => {
+      const { name, value } = e.target;
+  
+      setState({
+        ...state,
+        [name]: value
+      });
+    }
 
     const hideModal = (event) => {
         let modalContainer = document.querySelector('.modal-container');
@@ -26,6 +39,40 @@ const ModalComponent = (props) => {
                 <Icon>
                     <CloseCircleOutline />
                 </Icon>
+            </div>
+
+            <div className="main-container">
+                <h2> { props.title } </h2>
+                <form>
+                    <input
+                        type = 'text'
+                        name='taskName'
+                        value={ value }
+                        placeholder="Task Name"
+                        onChange={ changeText }
+                    />
+
+                    <textarea
+                        type = 'text'
+                        name='description'
+                        value={ value }
+                        placeholder="Please Add Description"
+                        onChange={ changeText }
+                    />
+                    
+                    <div className="btn-container">
+                        <button
+                            type='button'
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type='submit'
+                        >
+                            { props.action }
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
