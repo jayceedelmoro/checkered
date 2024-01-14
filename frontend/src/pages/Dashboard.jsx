a6d598eaa4776ff5081d181587262878cdb25192
Carlo <jancarlodelmoro@gmail.com>
2024-01-14 09:39:17 +0800

    Feat: Added Styling to Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom';
+
+import TaskComponent from '../components/TaskComponent';
+
+import '../styling/Dashboard.scss'
 
 const Dashboard = () => {
+    
+const taskData = useLoaderData();
+
   return (
-    <div>Dashboard</div>
+    <>
+
+        <div class="dashboard-container">
+            <div class="controls">
+                <div class="add-btn-container">
+                    <button
+                        class="add-task"
+                        onClick="modalToggle"
+                    >
+                        Add Task
+                    </button>
+                </div>
+            </div>
+
+            {/* In Progress Tasks */}
+            <TaskComponent title = 'In Progress'/>
+
+            {/* Completed */}
+            <TaskComponent title = 'Completed'/>
+        </div>
+    </>
   )
 }
 
